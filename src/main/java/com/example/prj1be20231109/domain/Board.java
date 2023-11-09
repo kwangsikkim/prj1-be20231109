d966ee6e1a51add2bed5fd5e51f56763ef5dad3a
kwangsik Kim <penggusic@gmail.com>
2023-11-09 15:20:30 +0900

    게시물 목록 응답, Board dto에 id, inserted 추가

--- src/main/java/com/example/prj1be20231109/domain/Board.java
+++ src/main/java/com/example/prj1be20231109/domain/Board.java
@@ -3,10 +3,14 @@ package com.example.prj1be20231109.domain;
 import lombok.Data;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.time.LocalDateTime;
+
 @Data
 public class Board {
+    private Integer id;
     private String title;
     private String content;
     private String writer;
+    private LocalDateTime inserted;
 
 }
