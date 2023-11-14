5ead059c7a0a65fe7f817ef550f3239cae0bf984
kwangsik Kim <penggusic@gmail.com>
2023-11-14 10:13:40 +0900

    글 쓸 때 작성자(writer) 확인 삭제

--- src/main/java/com/example/prj1be20231109/service/BoardService.java
+++ src/main/java/com/example/prj1be20231109/service/BoardService.java
@@ -28,11 +28,6 @@ public class BoardService {
         if (board.getTitle() == null || board.getTitle().isBlank()) {
             return false;
         }
-
-        if (board.getWriter() == null || board.getWriter().isBlank()) {
-            return false;
-        }
-
         return true;
     }
 
