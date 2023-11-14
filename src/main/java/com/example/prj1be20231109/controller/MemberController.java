68d40ba269973e5ae5ebbd47c12a8c858ec0acc9
kwangsik Kim <penggusic@gmail.com>
2023-11-14 09:45:39 +0900

    로그인 후 Member객체 session attribute "login" 추가

--- src/main/java/com/example/prj1be20231109/controller/MemberController.java
+++ src/main/java/com/example/prj1be20231109/controller/MemberController.java
@@ -5,8 +5,11 @@ import com.example.prj1be20231109.domain.Board;
 import com.example.prj1be20231109.domain.Member;
 import com.example.prj1be20231109.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.List;
 
@@ -89,6 +92,16 @@ public class MemberController {
             return ResponseEntity.ok().build();
         } else {
             return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PostMapping("login")
+    public ResponseEntity login(@RequestBody Member member, WebRequest request) {
+
+        if (service.login(member, request)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
 }
