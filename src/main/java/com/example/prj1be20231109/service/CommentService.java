0c11d5feae38f14c255b55b906f0c219b2d6ebed
kwangsik Kim <penggusic@gmail.com>
2023-11-16 10:21:43 +0900

    댓글 수정 시 권한/유효성 검사

--- src/main/java/com/example/prj1be20231109/service/CommentService.java
+++ src/main/java/com/example/prj1be20231109/service/CommentService.java
@@ -59,6 +59,20 @@ public class CommentService {
     public boolean update(Comment comment) {
         return mapper.update(comment) == 1;
     }
+
+    public boolean updateValidate(Comment comment) {
+        if (comment == null){
+        }
+
+        if (comment.getId() == null) {
+            return false;
+        }
+
+        if (comment.getComment() == null || comment.getComment().isBlank()) {
+            return false;
+        }
+        return true;
+    }
 }
 
 
