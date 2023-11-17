f0fa86e0a39d5814fde30667da3628254566429b
kwangsik Kim <penggusic@gmail.com>
2023-11-17 11:26:18 +0900

    게시물 목록에 좋아요 수 표시

--- src/main/java/com/example/prj1be20231109/domain/Board.java
+++ src/main/java/com/example/prj1be20231109/domain/Board.java
@@ -14,5 +14,6 @@ public class Board {
     private String nickName;
     private LocalDateTime inserted;
     private Integer countComment;
+    private Integer countLike;
 
 }
--- src/main/java/com/example/prj1be20231109/mapper/BoardMapper.java
+++ src/main/java/com/example/prj1be20231109/mapper/BoardMapper.java
@@ -17,15 +17,17 @@ public interface BoardMapper {
 
     @Select("""
             SELECT b.id,
-               b.title,
-               b.writer,
-               m.nickName,
-               b.inserted,
-               COUNT(c.id) countComment
-        FROM board b JOIN member m ON b.writer = m.id
-                     LEFT JOIN comment c on b.id = c.boardId
-        GROUP BY b.id
-        ORDER BY b.id DESC
+                   b.title,
+                   b.writer,
+                   m.nickName,
+                   b.inserted,
+                   COUNT(DISTINCT c.id) countComment,
+                    COUNT(DISTINCT l.id) countLike
+            FROM board b JOIN member m ON b.writer = m.id
+                         LEFT JOIN comment c on b.id = c.boardId
+                        LEFT JOIN boardLike l ON b.id = l.boardId
+            GROUP BY b.id
+            ORDER BY b.id DESC
             """)
     List<Board> selectAll();
 
